fd5e95275add7da207683affb78557aab697294b
shodan <jairferreiraneto1704mg@gmail.com>
2026-03-25 22:37:58 -0300

    Initial commit

---
+++ src/test/java/AreaDeTransferenciaTest.java
@@ -0,0 +1,26 @@
+package Singleton;
+
+import org.example.AreaDeTransferencia;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AreaDeTransferenciaTest {
+
+    @Test
+    public void testGaranteMesmaInstancia() {
+        AreaDeTransferencia area1 = AreaDeTransferencia.getInstancia();
+        AreaDeTransferencia area2 = AreaDeTransferencia.getInstancia();
+
+        assertSame(area1, area2);
+    }
+
+    @Test
+    public void testCompartilhamentoDeConteudo() {
+        AreaDeTransferencia area1 = AreaDeTransferencia.getInstancia();
+        area1.setConteudo("Texto copiado");
+
+        AreaDeTransferencia area2 = AreaDeTransferencia.getInstancia();
+
+        assertEquals("Texto copiado", area2.getConteudo());
+    }
+}
